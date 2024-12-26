129fc9f2e857023124ce7d012520ac43a678007d
wakky-alcedo <wakky.alcedo@gmail.com>
2024-12-27 03:31:39 +0900

    add(touchpanel): 明るさを表示するようにした

--- sandbox/touchpanel/src/main.cpp
+++ sandbox/touchpanel/src/main.cpp
@@ -2,15 +2,19 @@
 #define LGFX_USE_V1
 #include <LovyanGFX.hpp>
 
-class LGFX : public lgfx::LGFX_Device
-{
+#define PIN_LED_RED 4
+#define PIN_LED_GREEN 16
+#define PIN_LED_BLUE 17
+#define PIN_BRIGTHNESS 4
+#define PIN_SPEAKER 26
+
+class LGFX : public lgfx::LGFX_Device {
     lgfx::Panel_ILI9341 _panel_instance;
     lgfx::Bus_SPI       _bus_instance;
     lgfx::Light_PWM     _light_instance;
     lgfx::Touch_XPT2046 _touch_instance;
 public:
-    LGFX(void)
-    {
+    LGFX(void) {
         {
             auto cfg = _bus_instance.config();
 
@@ -93,54 +97,59 @@ public:
 };
 
 LGFX display;
+LGFX_Sprite sprite(&display);      // lcdに描画するスプライト作成
 
-void setup(void)
-{
+void setup(void) {
     display.init();
 
     display.setTextSize((std::max(display.width(), display.height()) + 255) >> 8);
 
-    if (display.touch())
-    {
-        if (display.width() < display.height()) display.setRotation(display.getRotation() ^ 1);
+    if (display.touch()) {
+        if (display.width() < display.height()) {
+            display.setRotation(display.getRotation() ^ 1); // 縦向きの場合は横向きにする
+        }
 
-        display.setTextDatum(textdatum_t::middle_center);
-        display.drawString("touch the arrow marker.", display.width() >> 1, display.height() >> 1);
-        display.setTextDatum(textdatum_t::top_left);
+        display.setTextDatum(textdatum_t::middle_center); // テキストの描画位置を設定
+        display.drawString("touch the arrow marker.", display.width() >> 1, display.height() >> 1); // メッセージを表示
+        display.setTextDatum(textdatum_t::top_left); // テキストの描画位置を設定
 
-        std::uint16_t fg = TFT_WHITE;
-        std::uint16_t bg = TFT_BLACK;
-        if (display.isEPD()) std::swap(fg, bg);
-        display.calibrateTouch(nullptr, fg, bg, std::max(display.width(), display.height()) >> 3);
+        // std::uint16_t fg = TFT_WHITE;
+        // std::uint16_t bg = TFT_BLACK;
+        // if (display.isEPD()) std::swap(fg, bg);
+        // display.calibrateTouch(nullptr, fg, bg, std::max(display.width(), display.height()) >> 3);
     }
 
     display.fillScreen(TFT_BLACK);
+
+    display.setBrightness(255); // 明るさの設定 なんかうまく反映されていなさそう？
+
+    // ピンの設定
+    pinMode(PIN_LED_RED, OUTPUT);
+    pinMode(PIN_LED_GREEN, OUTPUT);
+    pinMode(PIN_LED_BLUE, OUTPUT);
+    pinMode(PIN_BRIGTHNESS, INPUT);
+
+    analogWrite(PIN_LED_RED, 0); // 明るさが逆
+    analogWrite(PIN_LED_GREEN, 0);
+    analogWrite(PIN_LED_BLUE, 0);
+
+    sprite.createSprite(100, 20);
 }
 
 uint32_t count = ~0;
-void loop(void)
-{
+void loop(void) {
+
+
+    // 明るさを取得
+    uint16_t brightness = analogRead(34);
+
+
+    sprite.fillScreen(TFT_BLACK);
+    sprite.setTextColor(TFT_WHITE); // 文字色を設定
+    sprite.drawString("brightness", 0, 0);
+    sprite.drawNumber(brightness, 70, 0); // 画面の向きを表示
+
     display.startWrite();
-    display.setRotation(++count & 7);
-    display.setColorDepth((count & 8) ? 16 : 24);
-
-    display.setTextColor(TFT_WHITE);
-    display.drawNumber(display.getRotation(), 16, 0);
-
-    display.setTextColor(0xFF0000U);
-    display.drawString("R", 30, 16);
-    display.setTextColor(0x00FF00U);
-    display.drawString("G", 40, 16);
-    display.setTextColor(0x0000FFU);
-    display.drawString("B", 50, 16);
-
-    display.drawRect(30, 30, display.width() - 60, display.height() - 60, count * 7);
-    display.drawFastHLine(0, 0, 10);
-
-    display.endWrite();
-
-    int32_t x, y;
-    if (display.getTouch(&x, &y)) {
-        display.fillRect(x - 2, y - 2, 5, 5, count * 7);
-    }
+    sprite.pushSprite(30, 50);
+    display.endWrite(); // 描画を終了
 }
