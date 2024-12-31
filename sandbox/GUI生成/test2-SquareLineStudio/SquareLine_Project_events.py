a335541336bddbc4d96e38b0073f427c0a42506a
wakky-alcedo <wakky.alcedo@gmail.com>
2025-01-01 03:12:21 +0900

    add(lcd_test4-squarelinestudio): squarelinestudioを使ったテストプログラムを追加，それっぽい感じにUIができてきた

---
+++ sandbox/GUI生成/test2-SquareLineStudio/SquareLine_Project_events.py
@@ -0,0 +1,28 @@
+
+def SwitchLightClicked(event_struct):
+    return
+
+
+def SliderBrightnessChanged(event_struct):
+    return
+
+
+def SliderColorChanged(event_struct):
+    return
+
+
+def ButtonTempUpClicked(event_struct):
+    return
+
+
+def ButtonOpenClicked(event_struct):
+    return
+
+
+def ButtonStopClicked(event_struct):
+    return
+
+
+def ButtonCloseClicked(event_struct):
+    return
+
