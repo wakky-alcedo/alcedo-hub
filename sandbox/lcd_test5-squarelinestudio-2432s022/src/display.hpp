fa4058ce99abd8b527d6d762f989d186b5052868
wakky-alcedo <wakky.alcedo@gmail.com>
2025-02-03 00:30:00 +0900

    refactor(lcd): setup処理等を関数化

--- sandbox/lcd_test5-squarelinestudio-2432s022/src/display.hpp
+++ sandbox/lcd_test5-squarelinestudio-2432s022/src/display.hpp
@@ -106,4 +106,71 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
     }
 }
 
+/*setup the display*/
+void display_setup()
+{
+    touch.begin(); /*初始化触摸板*/
+
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.println( LVGL_Arduino );
+    Serial.println( "I am LVGL_Arduino" );
+
+    lv_init();
+
+    tft.begin();          /* TFT init */
+    tft.setRotation( 1 ); /* Landscape orientation, flipped */
+
+    lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
+
+    /*Initialize the display*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init( &disp_drv );
+    /*Change the following line to your display resolution*/
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register( &disp_drv );
+
+    /*Initialize the (dummy) input device driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init( &indev_drv );
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register( &indev_drv );
+
+    /*Initialize the UI*/
+    ui_init();
+    // Light
+    lv_obj_set_style_text_font(ui_LabelHome1, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelHome1, LV_SYMBOL_NEW_LINE);
+    // A/C
+    lv_obj_set_style_text_font(ui_LabelHome2, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelHome2, LV_SYMBOL_NEW_LINE);
+    lv_label_set_text( ui_LabelTempUp, LV_SYMBOL_UP);
+    lv_label_set_text( ui_LabelTempDown, LV_SYMBOL_DOWN);
+    lv_obj_set_style_text_font(ui_LabelACOn, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelACOn, LV_SYMBOL_POWER);
+    lv_obj_set_style_text_font(ui_LabelACOff, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    // lv_label_set_text( ui_LabelACOff, LV_SYMBOL_CLOSE);
+    lv_label_set_text( ui_LabelACOff, "OFF");
+    // Curtain
+    lv_obj_set_style_text_font(ui_LabelHome3, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelHome3, LV_SYMBOL_NEW_LINE);
+    lv_obj_set_style_text_font(ui_LabelOpen, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelOpen, LV_SYMBOL_LEFT LV_SYMBOL_RIGHT);
+    lv_obj_set_style_text_font(ui_LabelStop, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelStop, LV_SYMBOL_PAUSE);
+    lv_obj_set_style_text_font(ui_LabelClose, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text( ui_LabelClose, LV_SYMBOL_RIGHT LV_SYMBOL_LEFT);
+}
+
+/*Display loop*/
+void display_loop()
+{
+    lv_task_handler();
+}
+
 #endif // DISPLAY_HPP_
--- sandbox/lcd_test5-squarelinestudio-2432s022/src/main.cpp
+++ sandbox/lcd_test5-squarelinestudio-2432s022/src/main.cpp
@@ -18,79 +18,14 @@ void setup()
 {
     Serial.begin( 115200 ); /* prepare for possible serial debug */
 
-    touch.begin(); /*初始化触摸板*/
-
-    String LVGL_Arduino = "Hello Arduino! ";
-    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
-
-    Serial.println( LVGL_Arduino );
-    Serial.println( "I am LVGL_Arduino" );
-
-    lv_init();
-
-#if LV_USE_LOG != 0
-    lv_log_register_print_cb( my_print ); /* register print function for debugging */
-#endif
-
-    tft.begin();          /* TFT init */
-    tft.setRotation( 1 ); /* Landscape orientation, flipped */
-
-    lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
-
-    /*Initialize the display*/
-    static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init( &disp_drv );
-    /*Change the following line to your display resolution*/
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.draw_buf = &draw_buf;
-    lv_disp_drv_register( &disp_drv );
-
-    /*Initialize the (dummy) input device driver*/
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init( &indev_drv );
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = my_touchpad_read;
-    lv_indev_drv_register( &indev_drv );
-
-
-    ui_init();
-    // Light
-    lv_obj_set_style_text_font(ui_LabelHome1, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelHome1, LV_SYMBOL_NEW_LINE);
-    // A/C
-    lv_obj_set_style_text_font(ui_LabelHome2, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelHome2, LV_SYMBOL_NEW_LINE);
-    lv_label_set_text( ui_LabelTempUp, LV_SYMBOL_UP);
-    lv_label_set_text( ui_LabelTempDown, LV_SYMBOL_DOWN);
-    lv_obj_set_style_text_font(ui_LabelACOn, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelACOn, LV_SYMBOL_POWER);
-    lv_obj_set_style_text_font(ui_LabelACOff, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    // lv_label_set_text( ui_LabelACOff, LV_SYMBOL_CLOSE);
-    lv_label_set_text( ui_LabelACOff, "OFF");
-    // Curtain
-    lv_obj_set_style_text_font(ui_LabelHome3, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelHome3, LV_SYMBOL_NEW_LINE);
-    lv_obj_set_style_text_font(ui_LabelOpen, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelOpen, LV_SYMBOL_LEFT LV_SYMBOL_RIGHT);
-    lv_obj_set_style_text_font(ui_LabelStop, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelStop, LV_SYMBOL_PAUSE);
-    lv_obj_set_style_text_font(ui_LabelClose, &lv_font_montserrat_26, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_label_set_text( ui_LabelClose, LV_SYMBOL_RIGHT LV_SYMBOL_LEFT);
+    display_setup();
 
     Serial.println( "Setup done" );
 }
 
 void loop()
 {
-    lv_timer_handler(); /* let the GUI do its work */
-
-    // sprintf(chbuff, "%02d:%02d", tm->tm_hour, tm->tm_min);
-    // lv_label_set_text(uic_sample, chbuff);
-    // lv_label_set_text(uic_aa, chbuff);
-    // lv_label_get
-
+    display_loop();
     delay(5);
 }
 
