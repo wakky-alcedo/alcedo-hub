d7e455e6c46493d1814b1f9bad7d8de85da0ad0c
wakky-alcedo <wakky.alcedo@gmail.com>
2025-02-02 23:42:32 +0900

    fix(lcd): 表示はできた（タッチができない）

--- sandbox/lcd_test5-squarelinestudio-2432s022/src/display.hpp
+++ sandbox/lcd_test5-squarelinestudio-2432s022/src/display.hpp
@@ -2,28 +2,32 @@
 #ifndef DISPLAY_HPP_
 #define DISPLAY_HPP_
 
-class LGFX : public lgfx::LGFX_Device {
-    lgfx::Panel_ILI9341 _panel_instance;
-    lgfx::Bus_SPI       _bus_instance;
-    lgfx::Light_PWM     _light_instance;
-    lgfx::Touch_XPT2046 _touch_instance;
+#define SCR 8
+class LGFX : public lgfx::LGFX_Device
+{
+    lgfx::Panel_ST7789  _panel_instance;    // ST7789UI
+    lgfx::Bus_Parallel8 _bus_instance;      // MCU8080 8B
+    lgfx::Light_PWM _light_instance;
+    lgfx::Touch_FT5x06 _touch_instance;
+
 public:
-    LGFX(void) {
+    LGFX(void)
+    {
         {
             auto cfg = _bus_instance.config();
+            cfg.freq_write = 25000000;
+            cfg.pin_wr = 4;
+            cfg.pin_rd = 2;
+            cfg.pin_rs = 16;
 
-            // SPIバスの設定
-            cfg.spi_host         = VSPI_HOST;
-            cfg.spi_mode         = 0;
-            cfg.freq_write       = 40000000;
-            cfg.freq_read        = 16000000;
-            cfg.spi_3wire        = true;
-            cfg.use_lock         = true;
-            cfg.dma_channel      = SPI_DMA_CH_AUTO;
-            cfg.pin_sclk         = 14; // 変更
-            cfg.pin_mosi         = 13; // 変更
-            cfg.pin_miso         = 12; // 変更
-            cfg.pin_dc           =  2; // 変更
+            cfg.pin_d0 = 15;
+            cfg.pin_d1 = 13;
+            cfg.pin_d2 = 12;
+            cfg.pin_d3 = 14;
+            cfg.pin_d4 = 27;
+            cfg.pin_d5 = 25;
+            cfg.pin_d6 = 33;
+            cfg.pin_d7 = 32;
 
             _bus_instance.config(cfg);
             _panel_instance.setBus(&_bus_instance);
@@ -32,58 +36,24 @@ public:
         {
             auto cfg = _panel_instance.config();
 
-            cfg.pin_cs           =    15; // 変更
-            cfg.pin_rst          =    -1; // 変更
-            cfg.pin_busy         =    -1; // 変更
+            cfg.pin_cs = 17;
+            cfg.pin_rst = -1;
+            cfg.pin_busy = -1;
 
-            cfg.panel_width      =   240;
-            cfg.panel_height     =   320;
-            cfg.offset_x         =     0;
-            cfg.offset_y         =     0;
-            cfg.offset_rotation  =     0;
-            cfg.dummy_read_pixel =     8;
-            cfg.dummy_read_bits  =     1;
-            cfg.readable         =  true;
-            cfg.invert           = false;
-            cfg.rgb_order        = false;
-            cfg.dlen_16bit       = false;
-            cfg.bus_shared       = false; // 変更
+            cfg.panel_width = 240;
+            cfg.panel_height = 320;
+            cfg.offset_x = 0;
+            cfg.offset_y = 0;
+            cfg.offset_rotation = 0;
+            // cfg.dummy_read_pixel = 8;
+            // cfg.dummy_read_bits = 1;
+            cfg.readable = false;
+            cfg.invert = false;
+            cfg.rgb_order = false;
+            cfg.dlen_16bit = false;
+            cfg.bus_shared = true;
 
             _panel_instance.config(cfg);
-        }
-
-        {
-            auto cfg = _light_instance.config();
-
-            cfg.pin_bl = 21;              // 変更
-            cfg.invert = false;
-            cfg.freq   = 44100;           
-            cfg.pwm_channel = 7;
-
-            _light_instance.config(cfg);
-            _panel_instance.setLight(&_light_instance);
-        }
-
-        {
-            auto cfg = _touch_instance.config();
-
-            cfg.x_min      =  300;  // 変更
-            cfg.x_max      = 3900;  // 変更
-            cfg.y_min      = 3700;  // 変更
-            cfg.y_max      =  200;  // 変更
-            cfg.pin_int    = -1;    // 変更
-            cfg.bus_shared = false; // 変更
-            cfg.offset_rotation = 0;
-
-            cfg.spi_host = HSPI_HOST; // 変更
-            cfg.freq = 1000000;
-            cfg.pin_sclk = 25;        // 変更
-            cfg.pin_mosi = 32;        // 変更
-            cfg.pin_miso = 39;        // 変更
-            cfg.pin_cs   = 33;        // 変更
-
-            _touch_instance.config(cfg);
-            _panel_instance.setTouch(&_touch_instance);
         }
 
         setPanel(&_panel_instance);
