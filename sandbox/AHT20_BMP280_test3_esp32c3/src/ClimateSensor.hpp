a208458731b56346b36e4372be613ade46d78480
wakky-alcedo <wakky.alcedo@gmail.com>
2025-02-08 16:15:00 +0900

    refactor(温湿度): クラス化

---
+++ sandbox/AHT20_BMP280_test3_esp32c3/src/ClimateSensor.hpp
@@ -0,0 +1,55 @@
+#ifndef CLIMATESENSOR_HPP_
+#define CLIMATESENSOR_HPP_
+
+#include <Adafruit_BMP280.h>
+#include <Adafruit_AHTX0.h>
+
+struct ClimateData {
+    uint16_t temperature;
+    uint8_t humidity;
+    uint8_t pressure;
+};
+
+class ClimateSensor {
+    public:
+        ClimateSensor();
+        void begin();
+        void read(ClimateData& data);
+    private:
+        Adafruit_BMP280 bmp;
+        Adafruit_AHTX0 aht;
+};
+
+ClimateSensor::ClimateSensor() : bmp(), aht() {}
+
+void ClimateSensor::begin() {
+    // AHT10/AHT20の初期化
+    if (!aht.begin()) {
+        Serial.println(F("Could not find a valid AHT20 sensor, check wiring!"));
+        while (1);
+    } else {
+        Adafruit_Sensor *aht_temp;
+        aht_temp = aht.getTemperatureSensor();
+        aht_temp->printSensorDetails();
+        Adafruit_Sensor *aht_humidity;
+        aht_humidity = aht.getHumiditySensor();
+        aht_humidity->printSensorDetails();
+    }
+    // BMP280の初期化
+    if (!bmp.begin()) {
+        Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+        while (1);
+    }
+}
+
+void ClimateSensor::read(ClimateData& data) {
+    // AHT10/AHT20から温度と湿度を取得
+    sensors_event_t temp_event;
+    sensors_event_t humidity_event;
+    aht.getEvent(&temp_event, &humidity_event);
+    data.temperature = (uint16_t)((temp_event.temperature + bmp.readTemperature())/2.0f * 10); // AHT20とBMP280の温度を平均
+    data.humidity = (uint8_t)humidity_event.relative_humidity; // AHT20から湿度を取得
+    data.pressure = (uint8_t)(bmp.readPressure() / 100.0F); // BMP280から気圧を取得
+}
+
+#endif // CLIMATESENSOR_HPP_
--- sandbox/AHT20_BMP280_test3_esp32c3/src/main.cpp
+++ sandbox/AHT20_BMP280_test3_esp32c3/src/main.cpp
@@ -1,69 +1,36 @@
 #include <Arduino.h>
 
-#include <Adafruit_BMP280.h>
-#include <Adafruit_AHTX0.h>
+#include "ClimateSensor.hpp"
 
-Adafruit_AHTX0 aht;
-Adafruit_Sensor *aht_humidity, *aht_temp;
-
-Adafruit_BMP280 bmp;
-
+ClimateSensor sensor;
+ClimateData climate_data;
 
 void setup(void) {
     Serial.begin(115200);
+    Serial.flush();
 
     // AHT10/AHT20の初期化
-    while (!Serial)
-        delay(10); // will pause Zero, Leonardo, etc until serial console opens
+    // while (!Serial)
+    //     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-    Serial.println("Adafruit AHT10/AHT20 test!");
-
-    if (!aht.begin()) {
-        Serial.println("Failed to find AHT10/AHT20 chip");
-        while (1) {
-        delay(10);
-        }
-    }
-
-    Serial.println("AHT10/AHT20 Found!");
-    aht_temp = aht.getTemperatureSensor();
-    aht_temp->printSensorDetails();
-
-    aht_humidity = aht.getHumiditySensor();
-    aht_humidity->printSensorDetails();
-    
-    // BMP280の初期化
-    bmp.begin();
+    sensor.begin();
 }
 
-uint32_t count = ~0;
 void loop(void) {
-    // 温度と湿度を取得
-    sensors_event_t humidity, temp;
-    aht.getEvent(&humidity, &temp); // AHT21から湿度と温度の取得
-    float temperature1 = temp.temperature;
-    float relative_humidity = humidity.relative_humidity;
-    // aht_humidity->getEvent(&humidity);
-    // aht_temp->getEvent(&temp);
-
-    // 温度と気圧を取得
-    bmp.takeForcedMeasurement();
-    float temperature2 = bmp.readTemperature();
-    float pressure = bmp.readPressure() / 100.0F;
+    // センサからデータを取得
+    sensor.read(climate_data);
 
     // シリアルモニタに表示
-    Serial.print("Temperature1 = ");
-    Serial.print(temperature1);
+    Serial.print("Temperature = ");
+    Serial.print(climate_data.temperature/10.0f);
     Serial.println(" *C");
     Serial.print("Humidity = ");
-    Serial.print(relative_humidity);
+    Serial.print(climate_data.humidity);
     Serial.println(" % rH"); // humidity is measured in % relative humidity (% rH)
-    Serial.print("Temperature2 = ");
-    Serial.print(temperature2);
-    Serial.println(" *C");
     Serial.print("Pressure = ");
-    Serial.print(pressure);
+    Serial.print(climate_data.pressure);
     Serial.println(" hPa");
+    Serial.println("---------------------------------");
 
     // 1秒待つ
     delay(1000);
